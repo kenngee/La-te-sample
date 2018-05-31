35a1f74e1447471ac13bf603b8accd4f63a86b7d
kenngee <zsp@muho.tv>
2018-05-31 16:35:21 +0800

    View层简单构建

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,9 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Sample App</title>
-</head>
-<body>
-  <h1>主页</h1>
-</body>
-</html>
+@extends('layouts.default')
+@section('title', '首页')
+@section('content')
+	<h1>首页</h1>
+@stop
